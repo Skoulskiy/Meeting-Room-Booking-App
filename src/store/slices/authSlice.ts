e05584a56d14d8fcd76a9a7e023e24cf024cbc5c
Skoulskiy <ogurethcs@gmail.com>
2026-09-25 10:23:20 +0300

    feat: added firebase initialization and base redux store

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import type { PayloadAction } from '@reduxjs/toolkit'; 
+import type { User } from "../../types";
+
+interface authState {
+  user: User | null;
+  isLoading: boolean;
+  isAuthenticated: boolean;
+  error: string | null;
+}
+
+const initialState : authState = {
+  user: null,
+  isLoading: true,
+  isAuthenticated: false,
+  error: null
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User | null>) => {
+      state.user = action.payload;
+      state.isAuthenticated = !!action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action: PayloadAction<string | null>) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    }
+  }
+});
+
+export const { setUser, setLoading, setError } = authSlice.actions;
+export default authSlice.reducer;
